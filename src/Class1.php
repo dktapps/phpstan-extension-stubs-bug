8b51780b07d2b5ddcf0410994705d12ca2ebe3e0
Dylan K. Taylor <dktapps@pmmp.io>
2023-01-20 17:49:56 +0000

    User code for comparison

---
+++ src/Class1.php
@@ -0,0 +1,8 @@
+<?php
+
+class Class1 implements Interface1{
+
+	public function getThing() : int{
+		return 1;
+	}
+}
--- src/Main.php
+++ src/Main.php
@@ -3,6 +3,7 @@
 class Main{
 
 	public function test() : void{
+		\PHPStan\dumpType((new Class1())->getThing());
 		\PHPStan\dumpType((new \DateTime('now'))->getTimestamp());
 	}
 }
